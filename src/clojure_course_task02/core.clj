a00bdae704c212a283c6774f971b3f0deaca423b
kharevich_d <kharevich_d@Kharevich_D.iba>
2013-03-28 09:27:22 +0300

    get file list in dir

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,10 +1,17 @@
 (ns clojure-course-task02.core
-  (:gen-class))
+  (:gen-class)
+  (:import java.io.File))
+
+
+(defn get-files-list [dir]
+  (let[files (.listFiles dir)
+       dirs (filter #(.isDirectory %) files)]
+  (map #(.getName %) (filter #(.isFile %) files))))
 
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  (get-files-list (java.io.File. path)))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
@@ -12,7 +19,7 @@
 (defn -main [file-name path]
   (if (or (nil? file-name)
           (nil? path))
-    (usage)
+    (dorun(find-files "^core.+" "\\"))
     (do
       (println "Searching for " file-name " in " path "...")
       (dorun (map println (find-files file-name path))))))
