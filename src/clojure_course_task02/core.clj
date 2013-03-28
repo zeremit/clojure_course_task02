cd10e9e4b832a2ce8974a27115bd26255aa37329
kharevich_d <kharevich_d@Kharevich_D.iba>
2013-03-28 11:28:02 +0300

    beta 2

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -7,16 +7,16 @@
   (re-matches pattern name))
   )
 
-(defn get-files-list [fil dir]
+(defn get-files-list [fil dir predicate]
   (let[files (.listFiles dir)
        dirs (filter #(.isDirectory %) files)]
-  (def  fl(future (map #(.getName %) (filter #(and (.isFile %) (isMtch fil (.getName %))) files))))
-  (flatten (concat @fl (pmap #(get-files-list fil %) dirs)))))
+  (def  fl(future (map #(.getName %) (filter #(and (.isFile %) (predicate fil (.getName %))) files))))
+  (flatten (concat @fl (pmap #(get-files-list fil % predicate) dirs)))))
 
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
-  (get-files-list file-name (File. path)))
+  (get-files-list file-name (File. path) isMtch))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
@@ -24,7 +24,7 @@
 (defn -main [file-name path]
   (if (or (nil? file-name)
           (nil? path))
-    (dorun(map println (find-files "^core.+" "./")))
+    (dorun(map println (find-files "^core_.+" "./")))
     (do
       (println "Searching for " file-name " in " path "...")
       (dorun (map println (find-files file-name path))))))
