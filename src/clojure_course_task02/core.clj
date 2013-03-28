f8575825369f3fa4527c26927c0c528649ca97dd
kharevich_d <kharevich_d@Kharevich_D.iba>
2013-03-28 11:55:34 +0300

    beta 6

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -2,19 +2,19 @@
   (:gen-class)
   (:import java.io.File))
 
-(defn isMtch [pat name]
+(defn isMatch [pat name]
   (re-matches (re-pattern pat) name))
 
 (defn get-files-list [dir predicate]
   (let[files (.listFiles dir)
        dirs (filter #(.isDirectory %) files)]
-  (def  fl(future (map #(.getName %) (filter #(and (.isFile %) (predicate (.getName %))) files))))
+  (def  fl(future (vec (map #(.getName %) (filter #(and (.isFile %) (predicate (.getName %))) files)))))
   (flatten (concat @fl (pmap #(get-files-list % predicate) dirs)))))
 
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
-  (get-files-list (File. path) (partial isMtch file-name)))
+  (get-files-list (File. path) (partial isMatch file-name)))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
@@ -22,7 +22,7 @@
 (defn -main [file-name path]
   (if (or (nil? file-name)
           (nil? path))
-    (dorun(map println (find-files "^core.+" "./")))
+    (usage)
     (do
       (println "Searching for " file-name " in " path "...")
       (dorun (map println (find-files file-name path))))))
